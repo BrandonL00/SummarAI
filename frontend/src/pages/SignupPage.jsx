7e7f41c6b5f08d8bc410a0589851ae87d505f02b
Aaron Santhosh <aarontsanthosh@gmail.com>
2025-01-25 17:27:18 -0600

    fixed import

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import { useAuthStore } from '../store/useAuthStore';
-import Button from '@mui/material/Button';
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
