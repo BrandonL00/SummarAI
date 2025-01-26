8621ff9232977b862de2757e2ec2793be6d4a20b
Zavier Vega-Yu <118783475+zavayu@users.noreply.github.com>
2025-01-26 06:55:10 -0600

    gptStore functions

--- frontend/src/store/gptStore.js
+++ frontend/src/store/gptStore.js
@@ -3,7 +3,11 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 
 export const gptStore = create((set) => ({
+  currentPage: 1,
 
-    
+  summarizeUpTo: async (text, upTo) => {},
 
+  generateFlashCardsUpTo: async (text) => {},
+
+  summarizeCharacterUpTo: async (text, upTo) => {},
 }));
