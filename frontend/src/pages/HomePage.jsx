ef2a6d7694274023e9fdcd85cbe514f4d29cd501
Zavier Vega-Yu <118783475+zavayu@users.noreply.github.com>
2025-01-26 11:21:29 -0600

    final

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,17 +4,36 @@ import BookContainer from "../components/BookContainer.jsx";
 import Sidebar from "../components/Sidebar";
 import { pdfStore } from "../store/pdfStore.js";
 import { useStore } from "zustand";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-    const { selectedFile } = useStore(pdfStore);
+  const { selectedFile, setSelectedFile } = useStore(pdfStore);
+
+  const handleButtonPress = () => {
+    setSelectedFile(null);
+  };
+
   return (
-    <div className={`w-full h-screen ${
+    <div
+      className={`w-full h-screen relative ${
         selectedFile ? "overflow-auto" : "overflow-hidden"
-    }`}>
+      }`}
+    >
       <Navbar />
       <div className="flex">
         {selectedFile ? (
-          <BookContainer />
+          <>
+            <Link to="/">
+              <button onClick={handleButtonPress}>
+                <img
+                  src="/arrow_back.svg"
+                  alt="Back"
+                  className="absolute left-20 top-40 size-14 z-10"
+                />
+              </button>
+            </Link>
+            <BookContainer />
+          </>
         ) : (
           <>
             <Sidebar />
