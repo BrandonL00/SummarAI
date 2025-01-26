247caea6b3427727192510bb67d36c98ce3b6eb7
Zavier Vega-Yu <118783475+zavayu@users.noreply.github.com>
2025-01-25 18:36:26 -0600

    file upload, bookshelf, sidebar

---
+++ frontend/src/components/Bookshelf.jsx
@@ -0,0 +1,9 @@
+const Bookshelf = () => {
+  return (
+    <div className="bg-lightGray-50 h-screen w-4/5 rounded-3xl p-10">
+      <h1>Bookshelf</h1>
+    </div>
+  );
+};
+
+export default Bookshelf;
