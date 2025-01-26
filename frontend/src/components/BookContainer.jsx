3bb7e63104c89c1e67f9faefbef306a3b77172f7
Aaron Santhosh <aarontsanthosh@gmail.com>
2025-01-26 03:38:26 -0600

    Progress Bar Update

--- frontend/src/components/BookContainer.jsx
+++ frontend/src/components/BookContainer.jsx
@@ -1,13 +1,12 @@
 import { useStore } from 'zustand';
 import { pdfStore } from '../store/pdfStore';
-import PDF from './Pdf';
 import Book from './Book';
 
 
 
 const BookContainer = () => {
   const { selectedFile } = useStore(pdfStore);
-
+  
   return (
     <div className="w-full h-[105dvh] justify-center text-center bg-lightGray-50">
       <Book/>
